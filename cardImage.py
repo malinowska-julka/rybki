34be8d1f06f4743b21b3c20180e709f897303dd2
Julia Malinowska <malinowska.julka@gmail.com>
2024-09-12 15:08:49 +0200

    printing food images instead of text

--- cardImage.py
+++ cardImage.py
@@ -1,95 +1,12 @@
 from PIL import Image, ImageDraw, ImageFont
 import pandas as pd
-import random
 import csv
 import math
-
-random.seed(123)
-
-RYBKA_TEMP = 12
-CARD_WIDTH = 800
-CARD_HEIGHT = 1100
-UPPER_RECT_WID_START = 50
-UPPER_RECT_WID_END = 250
-UPPER_RECT_HEIGHT = 250
-ACTION_REC_HEIGHT_START = 800
-ACTION_REC_HEIGHT_END = ACTION_REC_HEIGHT_START + 180
-LENGTH_HEIGHT = 710
-LENGTH_WIDTH = 610
-OGON_HEIGHT = 280
-OGON_WIDTH = 35
-KRYJOWKA_HEIGHT = OGON_HEIGHT + 150
-KRYJOWKA_WIDTH = OGON_WIDTH - 10
-RYBKA_WID_LEFT = 50
-RYBKA_HEIGHT_UP = 181
-
-
-def separate_name(text):
-    tokens = text.split()
-    no_words = len(tokens)
-    return [
-        " ".join(tokens[0 : no_words // 2]),
-        " ".join(tokens[math.ceil(no_words / 2) :]),
-    ]
-
-
-def wysrodkowany_text(text, coordinates_start, coordinates_end, font):
-    width = coordinates_end[0] - coordinates_start[0]
-    height = coordinates_end[1]
-    font_width = font.getlength(text)
-    new_width = (width - font_width) / 2 + coordinates_start[0]
-
-    return (new_width, height)
-
-
-def food_draw(food):
-    ims = []
-    sep = []
-    coors_sep = []
-    dec = random.randint(0, 2)
-    if dec in (0, 1):
-        sep = "images/slash.png"
-    else:
-        sep = "images/plus.png"
-
-    for i in range(len(food_rodzaje)):
-        count = food.count(food_rodzaje[i])
-        if count > 1 or food_rodzaje[i] == "random":
-            sep = "images/plus.png"
-        for x in range(count):
-            ims.append("images/" + food_rodzaje[i] + ".png")
-    if len(ims) == 1:
-        coors = [(127, 177)]
-    elif len(ims) == 2:
-        coors = [(87, 177), (168, 177)]
-        if sep == "images/slash.png":
-            coors_sep = [(139, 177)]
-        else:
-            coors_sep = [(139, 189)]
-    else:
-        coors = [(62, 177), (127, 177), (192, 177)]
-        if sep == "images/slash.png":
-            coors_sep = [(113, 177), (174, 177)]
-        else:
-            coors_sep = [(113, 189), (174, 189)]
-
-    return ims, coors, sep, coors_sep
-
-
-def temp_draw(temp):
-
-    im = Image.open(temp)
-    if temp in ("images/zimne.png", "images/srednie.png", "images/hot.png"):
-        coors = (105, 60)
-    elif temp == "images/trzy_razem.png":
-        coors = (60, 25)
-    else:
-        coors = (60, 60)
-    return im, coors
-
+from functions import *
+from constants import *
 
 if __name__ == "__main__":
-    rybki = csv.DictReader(open("data/processed_rybki_final.csv"))
+    rybki = csv.DictReader(open("data/rybki_igor.csv"))  # processed_rybki_final.csv"))
     food_rodzaje = ["glony", "mrożony", "suchy", "żywy", "ślimaki", "random"]
     for rybka in rybki:
         if rybka["image_path"] != "":
@@ -120,8 +37,11 @@ if __name__ == "__main__":
             )
 
             # rybka print
-            img = Image.open("images/rybki_img/" + rybka["image_path"])
-            result.paste(img, (RYBKA_WID_LEFT, RYBKA_HEIGHT_UP))
+            try:
+                img = Image.open("images/rybki_img/" + rybka["image_path"])
+                result.paste(img, (RYBKA_WID_LEFT, RYBKA_HEIGHT_UP))
+            except:
+                print("Brak zdjęcia rybki")
 
             draw.line(
                 ((UPPER_RECT_WID_END + 1, 50), (CARD_HEIGHT, 50)), fill="DarkSlateGray"
@@ -135,13 +55,75 @@ if __name__ == "__main__":
                 10,
                 fill="DodgerBlue",
             )
-            draw.rectangle(
-                ((0, ACTION_REC_HEIGHT_START), (CARD_WIDTH, ACTION_REC_HEIGHT_END)),
-                fill="White",
+            # akcja print
+            match rybka["kolor_akcji"]:
+                case "GDY ZAGRYWASZ":
+                    draw.rectangle(
+                        (
+                            (0, ACTION_REC_HEIGHT_START),
+                            (CARD_WIDTH, ACTION_REC_HEIGHT_END),
+                        ),
+                        fill="PowderBlue",
+                    )
+                case "GDY AKTYWUJESZ":
+                    draw.rectangle(
+                        (
+                            (0, ACTION_REC_HEIGHT_START),
+                            (CARD_WIDTH, ACTION_REC_HEIGHT_END),
+                        ),
+                        fill="#A67B5B",
+                    )
+                case "RAZ MIĘDZY TURAMI":
+                    draw.rectangle(
+                        (
+                            (0, ACTION_REC_HEIGHT_START),
+                            (CARD_WIDTH, ACTION_REC_HEIGHT_END),
+                        ),
+                        fill="Pink",
+                    )
+                case "KONIEC RUNDY":
+                    draw.rectangle(
+                        (
+                            (0, ACTION_REC_HEIGHT_START),
+                            (CARD_WIDTH, ACTION_REC_HEIGHT_END),
+                        ),
+                        fill="DodgerBlue",
+                    )
+                case "RAZ W GRZE":
+                    draw.rectangle(
+                        (
+                            (0, ACTION_REC_HEIGHT_START),
+                            (CARD_WIDTH, ACTION_REC_HEIGHT_END),
+                        ),
+                        fill="Gray",
+                    )
+                case _:
+                    draw.rectangle(
+                        (
+                            (0, ACTION_REC_HEIGHT_START),
+                            (CARD_WIDTH, ACTION_REC_HEIGHT_END),
+                        ),
+                        fill="PowderBlue",
+                    )
+
+            tekst_akcji = rybka["kolor_akcji"] + ", " + rybka["akcja"]
+            tekst_akcji, coor_and_foods = insert_next_lines(tekst_akcji, font_name)
+            for change_food in coor_and_foods:
+                im = Image.open("images/" + change_food[1] + ".png")
+                result.paste(im, change_food[0], im)
+            draw.text(
+                (5, ACTION_HEIGHT_START),
+                tekst_akcji,
+                fill="Black",
+                font=font_name,
+                align="left",  # TODO CHANGE FONT FOR ACTIONS
             )
 
-            # nazwa print
+            # TODO REPLACE FOOD/EGGS NAMES TO IMAGES
+            # TODO tekst ustalona wysokosc w zależnosci od wielkosci ryby
+            # (stosunek rozmiaru tekstu do rozmiaru ryby???)
 
+            # nazwa print
             if len(long_name) == 0:
                 draw.text(coor_name, rybka["name"], fill="Black", font=font_name)
                 draw.text(coor_latin, rybka["latin_name"], fill="Gray", font=font_latin)
@@ -177,10 +159,10 @@ if __name__ == "__main__":
                 )
 
             # jedzenie print
-            images, coors, sep, coors_sep = food_draw(rybka["food"])
+            images, coors, sep, coors_sep = food_draw(rybka["food"], food_rodzaje)
             for i in range(len(images)):
                 im = Image.open(images[i])
-                result.paste(im, coors[i])
+                result.paste(im, coors[i], im)
             for i in range(len(coors_sep)):
                 hopsa = Image.open(sep)
                 result.paste(hopsa, coors_sep[i])
@@ -209,8 +191,8 @@ if __name__ == "__main__":
             result.paste(kryjowka, (KRYJOWKA_WIDTH, KRYJOWKA_HEIGHT), kryjowka)
 
             # jajeczka
-            ikra = Image.open("images/ikra.png")
-            result.paste(ikra, (KRYJOWKA_WIDTH - 10, KRYJOWKA_HEIGHT + 100))
+            ikra = Image.open("images/jajo.png")
+            result.paste(ikra, (KRYJOWKA_WIDTH - 10, KRYJOWKA_HEIGHT + 100), ikra)
 
             result.save("cards/draw_" + rybka["name"] + ".png")
 
@@ -218,4 +200,3 @@ if __name__ == "__main__":
     # TODO punkty
     # TODO ogon zmniejszyc i length i zmienic kolory
     # TODO background transparent albo moze jak kolizja to zmniejszaj obraz o pixel?
-    # TODO tekst akcji ujednolicic i potem dopiero printowac na karcie
